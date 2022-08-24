2f9534bf9e852c87bc521e3ba401c6e126215a5f
della2233 <112000429+della2233@users.noreply.github.com>
2022-08-24 15:08:32 -0500

    co

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 class Persona:
 
+# se definen las variables con su tipo de dato, para ingresarlo despues.
+
     nombre: str
     apellido_materno: str
     apellido_paterno: str
@@ -10,8 +12,12 @@ class Persona:
     estado_civil: str 
     grado_estudio: str
 
+#se crea un metodo(se le nombro atributo al azar), para poder llamarlo cuando sea necesario
+
     def __init__(self, atributo):
         self.atributo = atributo
+
+#se pide al usuario ingresar los datos con input.
 
     nombre = str(input("Escribe tu nombre: "))
     apellido_paterno = str(input("Escribe tu apellido paterno: "))
@@ -23,8 +29,13 @@ class Persona:
     estado_civil = str(input("Escribe tu estado civil: "))
     grado_estudio = str(input("Escribe tu grado de estudios: "))
 
+#se imprimen en pantalla los datos dados haciendo concatenaciones, los datos de tipo int o float se 
+#convierten a str con str(atributo).
+
     print("\nEl nombre es: " + nombre + "\nEl apellido paterno es: " + apellido_paterno +
      "\nEl apellido materno es: " + apellido_materno + "\nLa edad es: " + str(edad) +
       "\nEl genero es: " + genero + "\nLa estatura es: " + str(estatura) +
        "\nEl peso es: " + str(peso) + "\nEl estado civil es: " + estado_civil +
         "\nEl grado de estudios es: " + grado_estudio)
+
+hi
